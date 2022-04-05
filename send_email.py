40a1f0eb8a8c373f598b2991dfa931baca8e421c
Dan Stingaciu <dsstingaciu@gmail.com>
2022-04-05 02:48:22 -0400

    Added body of email retireval with order title

--- send_email.py
+++ send_email.py
@@ -1,21 +1,31 @@
-from typing import List
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 gmail_user = 'coe817finalproject@gmail.com'
 gmail_password = 'Test#2022'
-to = ['coe817finalproject2@gmail.com']
+to = 'coe817finalproject2@gmail.com'
 
 def send_email(body: str):
-    print(body)
+    print(type(body))
     subject = 'Order'
 
-    email_text = """\
-    From: %s,
-    To: %s,
-    Subject: %s
+    #email_text = """\
+    #From: %s,
+    #To: %s,
+    #Subject: %s
 
-    %s
-    """ % (gmail_user, ", ".join(to), subject, body)
+    #%s
+    #""" % (gmail_user, ", ".join(to), subject, body)
+
+    message = MIMEMultipart()
+    message['From'] = gmail_user
+    message['To'] = to
+    message['Subject'] = subject
+
+    message.attach(MIMEText(body))
+
+    email_text = message.as_string()
 
     try:
         smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
